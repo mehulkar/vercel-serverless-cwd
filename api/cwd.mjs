49ee29ec8d8f5e7e5136a84c64f305a2feb5ee89
Mehul Kar <mehul.kar@vercel.com>
2023-01-17 12:04:31 -0800

    Show both cwd and task root env var

--- api/cwd.mjs
+++ api/cwd.mjs
@@ -1,4 +1,7 @@
 /* eslint-env node */
 export default function (request, response) {
-  response.status(200).send(process.cwd());
+  response.status(200).send({
+    taskRoot: process.env.LAMBDA_TASK_ROOT,
+    cwd: process.cwd(),
+  });
 }
